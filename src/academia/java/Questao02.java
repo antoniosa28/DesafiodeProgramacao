15d40f4c898a369666c39130ff10f79d101324c3
Antonio Marcos Barbosa de Sá <antonio.sa@ufu.br>
2022-02-18 10:29:58 -0300

    Update Questao02.java

--- src/academia/java/Questao02.java
+++ src/academia/java/Questao02.java
@@ -9,19 +9,11 @@ public class Questao02 {
                 + "(?=.*[a-z])(?=.*[A-Z])"
                 + "(?=.*[!@#$%^&*()-+])"
                 + "(?=\\S+$).{6,}$";
-        /*
-         * Nesse momento obteremos uma instância de Pattern, através do * método
-         * static compile(String regex), o qual recebe uma String que é a
-         * expressção regular
-         */
+        
         Pattern p = Pattern.compile(senhaForte);
 
-        //O método find() da Classe Matcher tenta encontrar a próxima subsequência da sequência de entrada que encontra o padrão. Ele retorna um valor booleano mostrando o mesmo.
         if (!p.matcher (senha).find()) return false;
 
-        /*
-         * Aqui, atravÃ©s da instancia de Pattern, chamando o método * matcher() e passando a fonte de busca
-         */
         Matcher m = p.matcher(senha);
 
         return m.matches();
